9c87e4a3e9280a87244778dccaf47fbc519caf15
Kristen White <kw21872@email.vccs.edu>
2025-09-17 14:00:30 -0400

    Added comments to code to match the example one

--- Chapter2Exercise5.cpp
+++ Chapter2Exercise5.cpp
@@ -1,12 +1,20 @@
-
+/*
+Project Name: Chapter 2 Exercise 5 Average of Values
+File Name: Chapter2Exercise5.cpp
+Programmer: Kristen White
+Date: 9/17/25
+Requirements:
+Get the average of a series of five different values.
+*/
 // This program calculates the average of five values.
 
-#include <iostream>
-using namespace std;
+#include <iostream> // we will always need this library for writing
+// to the console and reading from the keyboard.
+using namespace std; // We use this so we can simplify commands
 
 int main()
 {
-    double value1, value2, value3, value4, value5;
+    double value1, value2, value3, value4, value5; // The values we average
     double sum, average;
 
     // Get the five values.
@@ -34,6 +42,6 @@ int main()
     //Display average.
     cout << "The average of the values is: " << average << endl;
 
-    return 0;
+    return 0; //It is best to have a return statement if the module is defined as returning a value.
 
 }
